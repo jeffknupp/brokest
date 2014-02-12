062834f7d47ebe7e3f4e8637fc45cdc2a3d67ceb
Jeff Knupp <jknupp@appnexus.com>
2014-02-12 14:57:15 -0500

    add monitoring

--- brokest.py
+++ brokest.py
@@ -34,14 +34,8 @@ def queue(runnable, *args, **kwargs):
 if __name__ == '__main__':
     context = zmq.Context()
 
-    t = zmq.devices.ProcessMonitoredQueue(zmq.ROUTER, zmq.DEALER, zmq.PUB)
+    t = zmq.devices.MonitoredQueue(zmq.ROUTER, zmq.DEALER, zmq.PUB)
     t.bind_in('tcp://127.0.0.1:7070')
     t.bind_out('tcp://127.0.0.1:7080')
     t.bind_mon('tcp://127.0.0.1:9090')
     t.start()
-
-    # We never get here
-    frontend.close()
-    backend.close()
-    context.term()
-
